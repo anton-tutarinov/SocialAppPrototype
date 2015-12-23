2a30324be0c49e1f980f1b211ed551721aba942d
Антон Тутаринов <anton.tutarinov@gmail.com>
2015-12-23 12:52:47 +0200

    Initial ui

---
+++ SocialAppPrototype/Utils/HttpService.swift
@@ -0,0 +1,72 @@
+import Foundation
+
+class HttpService {
+    enum HttpMethod: String {
+        case Get = "GET"
+        case Post = "POST"
+    }
+    
+    func request(url url: String, params: [String: String]?, method: HttpMethod,
+        completionHandler: (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void) {
+            let request = createRequest(url: url, params: params, method: method)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithRequest(request,
+                completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                    completionHandler(data: data, response: response, error: error)
+            })
+            task.resume()
+    }
+    
+    func requestSync(url url: String, params: [String: String]?, method: HttpMethod,
+        completionHandler: (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void) {
+            let request = createRequest(url: url, params: params, method: method)
+            let session = NSURLSession.sharedSession()
+            let semaphore: dispatch_semaphore_t = dispatch_semaphore_create(0)
+            let task = session.dataTaskWithRequest(request,
+                completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                    completionHandler(data: data, response: response, error: error)
+                    dispatch_semaphore_signal(semaphore);
+            })
+            task.resume()
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+    }
+    
+    private func createRequest(url url: String, params: [String: String]?, method: HttpMethod) -> NSMutableURLRequest {
+        var urlString = "\(url)"
+        let request: NSMutableURLRequest
+        
+        if method == HttpMethod.Post {
+            let requestURL = NSURL(string: urlString)!
+            request = NSMutableURLRequest(URL: requestURL)
+            
+            if (params != nil) {
+                let paramsString = paramsToString(params: params!)
+                request.HTTPBody = paramsString.dataUsingEncoding(NSUTF8StringEncoding)
+            }
+        } else {
+            if (params != nil) {
+                let paramsString = paramsToString(params: params!)
+                urlString += "?\(paramsString)"
+            }
+            
+            let requestURL = NSURL(string: urlString)!
+            request = NSMutableURLRequest(URL: requestURL)
+        }
+        
+        request.HTTPMethod = method.rawValue
+        return request
+    }
+    
+    private func paramsToString(params params: [String: String]) -> String {
+        var paramsString = String()
+        for (key, value) in params {
+            if paramsString.characters.count > 0 {
+                paramsString += "&"
+            }
+            
+            paramsString += "\(key)=\(value)";
+        }
+        
+        return paramsString
+    }
+}
--- SocialAppPrototype/ViewController.swift
+++ SocialAppPrototype/ViewController.swift
@@ -1,15 +1,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    @IBOutlet weak var keyboardSpaceConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: "hideKeyboard:")
+        self.view.addGestureRecognizer(tapGesture)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo,
+            let keyboardRect = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue() {
+                self.keyboardSpaceConstraint?.constant = keyboardRect.height
+                self.view.layoutIfNeeded()
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.keyboardSpaceConstraint?.constant = 0.0
+        self.view.layoutIfNeeded()
+    }
+    
+    func hideKeyboard(recognizer: UITapGestureRecognizer) {
+        messageTextField.resignFirstResponder()
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        
+        
+        return cell
+    }
+}
+
+//extension ViewController: UITableViewDelegate {
+//    
+//}
